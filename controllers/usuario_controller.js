f6dc9f2ecbda1b9574a99d444f7905407153d707
JaiverTosne <jaivertosne@misena.edu.co>
2023-03-01 21:05:59 -0600

    Controlador Usuario V.02

---
+++ controllers/usuario_controller.js
@@ -0,0 +1,34 @@
+const { QueryError } = require('sequelize');
+const Sequelize = require('sequelize');
+const pregunta = require('../models').pregunta;
+const rol = require('../models').rol;
+const categoria = require('../models').categoria
+const usuario = require('../models').usuario;
+const archivo = require('../models').archivo
+const sugerencia=require('../models').sugerencia
+const subcategoria =require('../models').categoria;
+
+module.exports={
+     ListarUsuario(req,res){
+          return usuario.findAll({})
+          .then(usuario=>res.status(200).send(usuario))
+          .catch(error=>res.status(400).send(error));
+      },  
+
+     CreateUsuario(req, res){
+          return usuario.create({
+               nombre: req.body.nombre,
+               apellido: req.body.apellido,
+               correo_electronico:req.body.correo_electronico,
+               contrasena: req.body.contrasena,
+               repetir_contrasena: req.body.repetir_contrasena,
+               telefono: req.body.telefono,
+               direccion: req.body.direccion,
+               estado: req.body.estado
+          })
+          
+          .then(agricultor => res.status(200).send(agricultor))
+          .catch(error => res.status(400).send(error))
+     },
+
+}
