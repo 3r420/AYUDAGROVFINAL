e964e443f61a0d943b084df91dca73c5f19c3be5
Andrescruzxr <apquinonez9@misena.edu.co>
2023-03-02 20:38:46 -0500

    seeders

---
+++ seeders/20230303005312-sugerencia.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('sugerencia', [{
+      descripcion: 'come mas papa',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }]);
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('sugerencia', null, {});
+  }
+
+
+};
