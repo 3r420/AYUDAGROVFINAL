39c7beb6637c16f5cc4ff761c87b539216f836c4
JaiverTosne <jaivertosne@misena.edu.co>
2023-03-02 21:16:47 -0600

    id_usuario

--- migrations/20230302021736-create-usuario.js
+++ migrations/20230302021736-create-usuario.js
@@ -3,7 +3,7 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('usuario', {
-      id: {
+      id_usuario: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
