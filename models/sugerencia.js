96e50c2ec78e821e0c0437146294ecf980ed133a
3r420 <ydereazo31@misena.edu.co>
2023-03-15 20:30:44 -0500

    relacion de la tabla subcategoria completa

--- models/sugerencia.js
+++ models/sugerencia.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      this.belongsTo(models.Subcategoria,{foreignKey:'subcategorias_id'})
     }
   }
   Sugerencia.init({
@@ -20,7 +21,8 @@ module.exports = (sequelize, DataTypes) => {
       primaryKey:true,
       type:DataTypes.INTEGER
     },
-    subcategorias_id: DataTypes.INTEGER,
+    subcategorias_id: {type:DataTypes.INTEGER,
+      references:{models:'Subcategoria',key:'id_subcategoria'}},
     descripcion: DataTypes.STRING,
     usuario_id: DataTypes.INTEGER
   }, {
